2e910fc6825cffbda0d35cc7d59f09050adb0869
gliadev <gliatem@gmail.com>
2025-10-14 14:47:15 +0200

    sin cuenta atras

--- src/App.jsx
+++ src/App.jsx
@@ -41,9 +41,7 @@ function App() {
     }
   }, []);
 
-  if (showSplash) {
-    return <IntroSplash onFinish={() => setShowSplash(false)} />;
-  }
+  
 
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
